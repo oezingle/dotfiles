bc77fa49e035237834ac397ab9ad32d4c39c92b0
oezingle <oezingle@gmail.com>
2022-12-10 03:04:17 -0800

    Improved performance for annotations for class

--- lib/30log.lua
+++ lib/30log.lua
@@ -1,10 +1,13 @@
-
 local class = require("lib.30log.30log")
 
---- Create a class
----@param name string the name of the class
----@param properties table? properties for the class - not instances!
----@return table the bound class object. use :init(), not :new()
-return function (name, properties)
-    return class(name, properties)
+if false then
+    --- Create a class
+    ---@param name string the name of the class
+    ---@param properties table? properties for the class - not instances!
+    ---@return table the bound class object. use :init(), not :new()
+    class = function(name, properties)
+        return class(name, properties)
+    end
 end
+
+return class
