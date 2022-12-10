a1c3a29d3f9275a8ee40349947ab458fa42a522c
oezingle <oezingle@gmail.com>
2022-12-10 03:02:45 -0800

    Added annotations for class

--- lib/30log.lua
+++ lib/30log.lua
@@ -1,4 +1,10 @@
 
 local class = require("lib.30log.30log")
 
-return class
+--- Create a class
+---@param name string the name of the class
+---@param properties table? properties for the class - not instances!
+---@return table the bound class object. use :init(), not :new()
+return function (name, properties)
+    return class(name, properties)
+end
