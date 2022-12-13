dbaddce956b0b0c3264daa44713afc06623094ee
oezingle <oezingle@gmail.com>
2022-12-12 23:19:07 -0800

    Going insane over making upower optional

--- src/taskbar/battery.lua
+++ src/taskbar/battery.lua
@@ -4,9 +4,9 @@ local lgi = require("lgi")
 local has_upower = pcall(lgi.require, "UPowerGlib")
 
 if not has_upower then
-    local print = require("agnostic.print")
+    -- local print = require("src.agnostic.print")
 
-    print("disabling battery widget: Install upower")
+    -- print("disabling battery widget: Install upower")
 
     return function() end
 end
