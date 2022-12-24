2e570b31107f154d1d492970a4991c81ea7d18b4
oezingle <oezingle@gmail.com>
2022-12-24 15:10:09 -0800

    cmd_slider in one file

--- src/widgets/components/cmd_slider.lua
+++ src/widgets/components/cmd_slider.lua
@@ -1,10 +1,11 @@
 local wibox = require("wibox")
 local gears = require("gears")
-local awful = require("awful")
+local gcolor = gears.color
+local gtimer = gears.timer
 
 local shapes                = require("src.util.shapes")
 local bind_width_and_height = require("src.widgets.helper.bind_width_and_height")
-local create_slider_bg      = require("src.widgets.helper.create_slider_bg")
+local spawn                 = require("src.agnostic.spawn")
 
 local update_widget = require("src.widgets.helper.update")
 local config = require("config")
@@ -14,6 +15,31 @@ local no_scroll = require("src.widgets.helper.no_scroll")
 local config_dir = gears.filesystem.get_configuration_dir()
 
 local function do_nothing(...) end
+
+-- I don't need no stinking handle!
+---@param w table slider widget to create handle for
+local function create_slider_bg(w)
+    local value = w.value
+
+    local maximum = w.maximum
+    local minimum = w.minimum
+
+    local percent_pos = (value - minimum) / (maximum - minimum)
+
+    return gcolor {
+        type = "linear",
+
+        from = { 0, 1 },
+        to   = { w.width, 1 },
+
+        stops = {
+            { 0, config.button.hover },
+            { percent_pos, config.button.hover },
+            { percent_pos, config.button.normal },
+            { 1, config.button.normal },
+        }
+    }
+end
 
 local function cmd_slider(args)
     args = args or {}
@@ -46,7 +72,7 @@ local function cmd_slider(args)
 
     -- timer that gets constantly cancelled and re-inited so that
     -- command calls are throttled
-    local submit_change = gears.timer {
+    local submit_change = gtimer {
         timeout = 0.5,
         single_shot = true,
         callback = function()
@@ -101,7 +127,8 @@ local function cmd_slider(args)
         is_pressing = true
     end))
 
-    return update_widget {
+    --[[
+        return update_widget {
         widget = stack,
         update_callback = function()
             -- Let the user take the wheel
@@ -109,7 +136,7 @@ local function cmd_slider(args)
                 return
             end
 
-            awful.spawn.easy_async_with_shell(update_command, function(result)
+            spawn(update_command, function(result)
                 -- Check again in case there was a state change during execution time
                 if is_pressing then
                     return
@@ -124,6 +151,39 @@ local function cmd_slider(args)
         end,
         timeout = 0.5
     }
+    ]]
+
+    -- timer to update the visual
+    gtimer {
+        timeout = 0.5,
+        call_now = true,
+        autostart = true,
+        callback = function()
+            if not stack.visible then
+                return
+            end
+
+            -- Let the user take the wheel
+            if is_pressing then
+                return
+            end
+
+            spawn(update_command, function(result)
+                -- Check again in case there was a state change during execution time
+                if is_pressing then
+                    return
+                end
+
+                local asnumber = tonumber(result)
+
+                if type(asnumber) ~= "nil" then
+                    slider.value = asnumber
+                end
+            end)
+        end
+    }
+
+    return stack
 end
 
 return cmd_slider
