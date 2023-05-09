6cb554fdbd54cfdcdd35dce32b08e540c70eab99
oezingle <oezingle@gmail.com>
2023-05-08 21:23:17 -0700

    slay check_dependencies cache

--- src/sh/check_dependencies.lua
+++ src/sh/check_dependencies.lua
@@ -1,23 +1,56 @@
-
 local spawn = require("src.agnostic.spawn.promise")
+local Promise = require("src.util.Promise")
+
+-- TODO new version where dependencies are stored in a true/false table and if a false is found,
+-- TODO the function calls Promise.resolve(false), and then calls which for every dependency. If
+-- TODO a true is found, save it and move onto the next, whereas a false resolves false
+
+---@type table<string, boolean>
+local dependencies = {}
+
+---@param dependency string
+---@return Promise<boolean>
+local function check_dependency(dependency)
+    if dependencies[dependency] == true then
+        return Promise.resolve(true)
+    elseif dependencies[dependency] == false then
+        return Promise.resolve(false)
+    else
+        local cmd = string.format("which %s > /dev/null && printf y", dependency)
+
+        return spawn(cmd)
+            :after(function(response)
+                return response == "y" or response == "y\n"
+            end)
+            :after(function(found)
+                dependencies[dependency] = found
+
+                return found
+            end)
+    end
+end
 
 --- Check if dependencies are installed, returning a promise of if they are
 ---@param dependencies string|string[]
 ---@return Promise<boolean>
-local function check_dependencies (dependencies)
-    if type(dependencies) == "string" then dependencies = { dependencies} end
+local function check_dependencies(dependencies)
+    if type(dependencies) == "string" then dependencies = { dependencies } end
 
-    local lines = {}
-    
+    local checks = {}
+
     for _, dependency in ipairs(dependencies) do
-        table.insert(lines, string.format("which %s > /dev/null", dependency))
+        table.insert(checks, check_dependency(dependency))
     end
 
-    local cmd = table.concat(lines, " && ") .. " && printf success"
+    return Promise.all(checks)
+        :after(function (checks) 
+            for _, check in ipairs(checks) do
+                if not check[1] then
+                    return false 
+                end
+            end
 
-    return spawn(cmd)
-        :after(function (response)
-            return response == "success" or response == "success\n"
+            return true
         end)
 end
 
--- test/sh/check_dependencies.lua
+++ test/sh/check_dependencies.lua
@@ -18,16 +18,28 @@ test.suite(
         end
     end, "check sh"),
     test.test(function()
-        local has_sh = false
+        local has_dependencies = false
 
         check_dependencies({ "sh", "bash" })
             :after(function(met)
-                has_sh = met
+                has_dependencies = met
             end)
 
         -- check_dependencies works in async under AwesomeWM
         if not test.has_awesome() then
-            assert(has_sh)
+            assert(has_dependencies)
         end
-    end, "check sh and bash")
+    end, "check sh and bash"),
+    test.test(function ()
+        local has_dependencies
+        
+        check_dependencies({ "/does/not/exist", "/not/a/real/path", "sh" })
+            :after(function (met)
+                has_dependencies = met
+            end)
+
+        if not test.has_awesome() then
+            assert(not has_dependencies)
+        end
+    end, "check nonexistent dependencies")
 )
